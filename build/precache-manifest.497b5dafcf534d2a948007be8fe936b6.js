82bded1b470019417926d6ba0da837811ffb0f3e
apkwan <apkwan317@gmail.com>
2020-08-09 15:47:15 -0700

    nav bar update

---
+++ build/precache-manifest.497b5dafcf534d2a948007be8fe936b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31405f844ba961f309888186e0b61385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "715fa1064fb3378f1756",
+    "url": "/static/css/main.e0c8ff3c.chunk.css"
+  },
+  {
+    "revision": "b0efe92b5aef0d056622",
+    "url": "/static/js/2.e003b1be.chunk.js"
+  },
+  {
+    "revision": "b42f36b5d572c611b888796d07d4a679",
+    "url": "/static/js/2.e003b1be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715fa1064fb3378f1756",
+    "url": "/static/js/main.8b812e38.chunk.js"
+  },
+  {
+    "revision": "8b70eb2e35dee98fb26d",
+    "url": "/static/js/runtime-main.d33b66ca.js"
+  }
+]);
